9bc643ebfa38677414d3c661e6f6927c5ac95a17
Johannes Schirm <9149359+jojosoft@users.noreply.github.com>
2022-12-22 18:23:10 +0100

    Add field for editing target URL and introduce partial matching
    Another input field had to be added to the popup.
    While doing this, I also tried to improve the UI's CSS.
    It was at least needed to change flex-direction to colum.

--- background.js
+++ background.js
@@ -2,8 +2,9 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   if (changeInfo.status == 'complete') {
     chrome.tabs.get(tabId, function(tab) {
       chrome.storage.sync.get(null, function(theValue) {
-        if (theValue[tab.url] != undefined) {
-          chrome.tabs.executeScript(tabId, {code: theValue[tab.url]});
+        var url = getLongestURLMatch(theValue, tab.url);
+        if (url.length > 0) {
+          chrome.tabs.executeScript(tabId, {code: theValue[url]});
         }
       });
     });
